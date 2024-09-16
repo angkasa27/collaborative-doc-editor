102ba9d6e73683a0aa18864ddbdb47219b6495cf
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2024-09-16 19:38:02 +0700

    Add auth page

--- app/documents/[id]/page.tsx
+++ app/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Documents = () => {
@@ -9,6 +10,12 @@ const Documents = () => {
         <div className="flex w-fit items-center justify-center gap-2">
           <p className="document-title">This is a fake document title</p>
         </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor />
     </div>
