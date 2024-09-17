79b7258dec7e294b8f80e0a01e4da048eda6b530
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2024-09-17 09:59:30 +0700

    Add document editor and docuemnt list

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,97 @@
+"use server";
+
+import { nanoid } from "nanoid";
+import { liveblocks } from "../liveblocks";
+import { revalidatePath } from "next/cache";
+import { parseStringify } from "../utils";
+
+export const createDocument = async ({
+  userId,
+  email,
+}: CreateDocumentParams) => {
+  const roomId = nanoid();
+
+  try {
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: "Untitled Document",
+    };
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ["room:write"],
+    };
+
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: ["room:write"],
+    });
+
+    revalidatePath("/");
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error creating room: ${error}`);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    // TODO: Bring it on
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if (!hasAccess) {
+    //   throw new Error("You do not have access to this document");
+    // }
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log("Error happen when getting the document");
+  }
+};
+
+export const updateDocument = async ({
+  roomId,
+  title,
+}: {
+  roomId: string;
+  title: string;
+}) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    });
+    revalidatePath(`/documents/${roomId}`);
+    return parseStringify(updatedRoom);
+  } catch (error) {
+    console.log(`Error happen when updating the room: ${error}`);
+  }
+};
+
+export const getDocuments = async ({ email }: { email: string }) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+
+    // TODO: Bring it on
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if (!hasAccess) {
+    //   throw new Error("You do not have access to this document");
+    // }
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error happen when getting the documents: ${error}`);
+  }
+};
